86ff4df51f7463717955e2ce6b0831b57feca2e8
TamaraPerteghella <perteghella.tamara@libero.it>
2024-10-22 17:02:30 +0200

    Create get_read_length.sh

---
+++ data_preprocessing/get_read_length.sh
@@ -0,0 +1,22 @@
+# ---------------------------------------------------
+# Read length
+# ---------------------------------------------------
+
+for capture in Mv2 MpreCap Hv3 HpreCap
+do
+    while read lab cap tmp sample
+    do
+        path="${lab}_${cap}_${tmp}_${sample}.gff.gz" #Path to the read mapping GFF
+        details=$(echo -e "$lab\t$cap\t$tmp\t$sample")
+        if [ -f $path ]; then
+            zcat $path | egrep -v "SIRVome_isoforms|ERCC" | awk -F"\t" -v OFS="\t" -v det="$details" '{ split($9, ids, "\""); print $5-$4, ids[2], det }'
+        fi
+    done < samples.gencode.${capture}.tsv > gencode.readlength.${capture}.tsv
+done
+
+for capture in Mv2 MpreCap Hv3 HpreCap
+do
+    awk -F'\t' '{key = "\"" $2 "\"" "\t" $3 "\t" $4 "\t" $5 "\t" $6; sums[key]+=$1} END {for (key in sums) print key "\t" sums[key]}' gencode.readlength.$capture.tsv > gencode.readlength.$capture.finite.tsv
+done
+
+for i in gencode.readlength.*finite.tsv; do singularity exec ~/docker_images/r4_gencode_phase3_latest.sif Rscript get_length_distribution.R $i; done
