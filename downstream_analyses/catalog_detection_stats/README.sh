dab3cf46e85fe361d5dcb3164c9d4c5a81e699ef
Gazal90 <gazaldeep.gk@gmail.com>
2025-03-11 09:24:33 +0100

    chnaged script names to be more apt

--- downstream_analyses/catalog_detection_stats/README.sh
+++ downstream_analyses/catalog_detection_stats/README.sh
@@ -1,13 +1,12 @@
 ####Target regions detected per target class/catalog (barplot)
-#---Plots the % target regions detected overall, further differentiating the novel and known regions, in addition to displaying the number of catalog regions and transcripts (CLS3 transcripts/intron chains)
+#---Plots the % target regions detected overall, further differentiating the novel and known regions, in addition to displaying the number of catalog regions and transcripts
 
-bash rcd_catalogDetectionStats.sh
-
+bash catalogDetectionStats.sh
 #final plot -> https://docs.google.com/presentation/d/1S4pvtA8Lpl8P_fMwfMA2GnKOKp8mIoZaAQWCFaMuGiw/edit#slide=id.g30a87e2963f_0_0
 
 ####Novel transcripts detected per target class per tissue (matrices)
-#---Plots novel transcripts (CLS3 transcripts/intron chains) as well as the proportion of target regions that help detect novel transcripts, thanks to the target catalog
+#---Plots transcripts (CLS intron chains) as well as the proportion of target regions detected thanks to the target catalog
 
-bash rcd_catalogDetection_perTissue.sh
+bash catalogDetection_perTissue.sh
 #final plots: https://docs.google.com/presentation/d/1S4pvtA8Lpl8P_fMwfMA2GnKOKp8mIoZaAQWCFaMuGiw/edit#slide=id.g2ffe14a0607_0_472 
 #https://docs.google.com/presentation/d/1S4pvtA8Lpl8P_fMwfMA2GnKOKp8mIoZaAQWCFaMuGiw/edit#slide=id.g2fa50d3fcc5_1_16
